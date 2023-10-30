72ac622d4eb898977dd815f340e5c1f13c7b650c
paulo07marcena <paulo.marcena@sptech.school>
2023-10-30 11:10:02 -0300

    Analytics 30.10.23

---
+++ R - Analytics/Analytics 30.10.23.R
@@ -0,0 +1,82 @@
+pop_sol_cred <- 10000
+
+set.seed(999)
+
+n <- 6048
+
+set.seed(999)
+
+bom.n <- rbinom(n,1,.85)
+
+bom.n
+
+table(bom.n)
+
+str(bom.n)
+
+mean(bom.n)
+
+class_pg <- factor(bom.n,
+            levels = c(0,1),
+            labels = c("mau","bom"),
+            ordered = TRUE
+            )
+
+summary(class_pg)
+
+table(class_pg)
+
+data.default<-data.frame(class_pg)
+
+data.default
+
+tail(data.default)
+
+head(data.default)
+
+data.default <- data.frame(table(class_pg))
+
+data.default
+
+colnames(data.default)[2]<-"Freq_absoluta"
+
+colnames(data.default)[1] <- "Status"
+
+data.default<-data.frame(id=1:2,data.default)
+
+Freq_relativa<-
+
+round(data.default$Freq_absoluta/sum(data.default$Freq_absoluta)*100,2)
+
+Freq_relativa<-data.frame(id=1:2,Freq_relativa)
+
+table_padrão<-data.frame(merge(data.default,Freq_relativa))
+
+barplot(table_padrão$Freq_relativa,xlab= "Tipo de Tomador
+Crédito",ylab="Frequência_Relativa",col=c("seagreen", "yellowgreen"))
+
+class_pg
+
+prop.table(table(class_pg))
+
+barplot(prop.table(table(class_pg)),xlab="Tipo de tomador de
+crédito",ylab="Frequência Absoluta",col=c("red","blue"))
+
+set.seed(123)
+idade <- round(rnorm(n,35,5),0)
+
+set.seed(123)
+idade <- round(rnorm(n,35,5),0)
+
+set.seed(123)
+sexo <- rbinom(n,1,.85)
+sexo <- factor(sexo,
+                 levels = c(0,1),
+                 labels = c("Homem","Mulher"),
+                 ordered = TRUE
+)
+
+
+renda <- round(rnorm(n,2000.00, 250),2)
+
+dados <- data.frame(sexo, renda, idade)
