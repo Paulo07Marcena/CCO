2bb74bc7526ddf9a97f01fee29cd1bf0039f7ef8
paulo07marcena <paulo.marcena@sptech.school>
2023-11-09 11:02:06 -0300

    Tipos primitivos

---
+++ Java/atividade_em_aula_09.11.23/src/Exercicio01/DaysOfWeek.java
@@ -0,0 +1,12 @@
+package Exercicio01;
+
+public class DaysOfWeek {
+
+    public static final String MONDAY = "Segunda-feira";
+    public static final String TUESDAY = "Terça-feira";
+    public static final String WEDNESDAY = "Quarta-feira";
+    public static final String THURSDAY = "Quinta-feira";
+    public static final String FRIDAY = "Sexta-feira";
+    public static final String SATURDAY = "Sábado";
+    public static final String SUNDAY = "Domingo";
+}
---
+++ Java/atividade_em_aula_09.11.23/src/Exercicio01/Main.java
@@ -0,0 +1,24 @@
+package Exercicio01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        Test test1 = new Test();
+
+        test1.setNome("Caio");
+        test.setNome("Paulo");
+
+        System.out.println(test.getNome());
+        System.out.println(test1.getNome());
+        System.out.println(Test.NOME_COMPLETO);
+
+        int result = Test.sum(1,6);
+
+        System.out.println(result);
+        System.out.println(DaysOfWeek.MONDAY);
+
+    }
+
+}
